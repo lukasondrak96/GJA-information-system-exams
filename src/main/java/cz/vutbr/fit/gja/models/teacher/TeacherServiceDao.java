e7f8e718b1d737ac7f61da1a5698cb1efe875be6
Ondřej Novák <ondrej.novak96@email.cz>
2020-01-09 15:42:12 +0100

    Commented teacher package.

--- src/main/java/cz/vutbr/fit/gja/models/teacher/TeacherServiceDao.java
+++ src/main/java/cz/vutbr/fit/gja/models/teacher/TeacherServiceDao.java
@@ -1,12 +1,38 @@
 package cz.vutbr.fit.gja.models.teacher;
 
+/**
+ * ServiceDao class for Teacher
+ */
 public interface TeacherServiceDao {
 
+    /**
+     * Save specific teacher to database
+     *
+     * @param teacher teacher to save
+     */
     void saveTeacher(Teacher teacher);
 
+    /**
+     * Checks, if the teacher is saved in the database
+     *
+     * @param teacher specific teacher
+     * @return true if teacher is already in database, false if not in database
+     */
     boolean isTeacherAlreadySaved(Teacher teacher);
 
+    /**
+     * Checks, if password and repeated password of teacher are same
+     *
+     * @param teacher specific teacher
+     * @return true if passwords are same, false if passwords are different
+     */
     boolean isPasswordSame(Teacher teacher);
 
+    /**
+     * Gets specific teacher from database
+     *
+     * @param email email of teacher
+     * @return specific teacher
+     */
     Teacher getTeacher(String email);
 }
