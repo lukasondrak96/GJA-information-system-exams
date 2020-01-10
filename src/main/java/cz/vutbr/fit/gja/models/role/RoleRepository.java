7a2f61b6619f7692e887d483150e56b438cb9250
Tomáš Chocholatý <tom.chocholaty@seznam.cz>
2020-01-10 11:09:30 +0100

    Added comment to model RoleRepository.

--- src/main/java/cz/vutbr/fit/gja/models/role/RoleRepository.java
+++ src/main/java/cz/vutbr/fit/gja/models/role/RoleRepository.java
@@ -3,7 +3,15 @@ package cz.vutbr.fit.gja.models.role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Repository class of Role
+ */
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Integer> {
+    /**
+     * Find Role by user role
+     * @param role role of user
+     * @return Role if exist, null otherwise
+     */
     Role findByRoleName(String role);
 }
