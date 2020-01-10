798b8bff83a714deeae0768cfcdbf884b008949f
Tomáš Chocholatý <tom.chocholaty@seznam.cz>
2020-01-10 11:07:12 +0100

    Added comment to model role.

--- src/main/java/cz/vutbr/fit/gja/models/role/Role.java
+++ src/main/java/cz/vutbr/fit/gja/models/role/Role.java
@@ -3,24 +3,42 @@ package cz.vutbr.fit.gja.models.role;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
+/**
+ * This class represent the entity of Role
+ */
+
 @Entity
 @Table(name = "role")
 public class Role {
+    /**
+     * Role ID
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id_role")
     private int idRole;
 
+    /**
+     * Type of roll
+     */
     @NotNull
     @Column(name = "role_name")
     private String roleName;
 
+    /**
+     * Type of roll for system check
+     */
     @Column(name = "role_desc")
     private String roleDesc;
 
     public Role() {
     }
 
+    /**
+     * Creates a new Student
+     * @param roleName type of role
+     * @param roleDesc type of role for system check
+     */
     public Role(@NotNull String roleName, String roleDesc) {
         this.roleName = roleName;
         this.roleDesc = roleDesc;
