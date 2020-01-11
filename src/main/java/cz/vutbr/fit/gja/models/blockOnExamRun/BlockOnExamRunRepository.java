dfe99108c8681629e09d24b7972efbf655e33fa4
Tomáš Líbal <tomas.libal123@gmail.com>
2020-01-11 15:30:24 +0100

    doc: block on exam run package

--- src/main/java/cz/vutbr/fit/gja/models/blockOnExamRun/BlockOnExamRunRepository.java
+++ src/main/java/cz/vutbr/fit/gja/models/blockOnExamRun/BlockOnExamRunRepository.java
@@ -12,8 +12,18 @@ import java.util.List;
  */
 @Repository
 public interface BlockOnExamRunRepository extends JpaRepository<BlockOnExamRun, Integer> {
+    /**
+     * Gets seating plan of given exam run
+     * @param examRun Exam run
+     * @return Seating plan of exam run
+     */
     List<BlockOnExamRun> findAllByExamRunReference(ExamRun examRun);
 
+    /**
+     * Gets all exams in which the student is present
+     * @param studentLogin Login of searched student
+     * @return Exam runs in which the student is present
+     */
     @Query("select er from BlockOnExamRun b join b.examRunReference er join b.studentReference s where s.login = ?1")
     List<ExamRun> getAllStudentExams(String studentLogin);
 }
