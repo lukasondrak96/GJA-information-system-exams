581c1a79e4dc993f46e89a2a3b8c08222d6e0518
Tomáš Líbal <tomas.libal123@gmail.com>
2020-01-11 19:17:52 +0100

    doc: exam run package

--- src/main/java/cz/vutbr/fit/gja/models/examRun/ExamRun.java
+++ src/main/java/cz/vutbr/fit/gja/models/examRun/ExamRun.java
@@ -8,10 +8,16 @@ import org.hibernate.annotations.OnDeleteAction;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
+/**
+ * This class represent the entity of Exam Run
+ */
 @Entity
 @Table(name = "exam_run")
 public class ExamRun {
 
+    /**
+     * Exam Run ID
+     */
     @Id
     @NotNull(message="Id exam runu je povinné")
     @SequenceGenerator(name = "ExamRunIdGenerator", sequenceName = "EXAM_RUN_SEQUENCE", allocationSize = 1)
@@ -19,23 +25,38 @@ public class ExamRun {
     @Column(name = "id_exam_run")
     private int idExamRun;
 
+    /**
+     * Date of exam run
+     */
     @NotNull(message="Datum je povinné")
     @Column(name = "exam_date")
     private String examDate;
 
+    /**
+     * Time when exam run starts
+     */
     @NotNull(message="Začátek zkoušky je povinný")
     @Column(name = "start_time")
     private String startTime;
 
+    /**
+     * Time when exam run ends
+     */
     @NotNull(message="Konec zkoušky je povinný")
     @Column(name = "end_time")
     private String endTime;
 
+    /**
+     * Reference to the exam to which the exam run relates
+     */
     @ManyToOne(fetch = FetchType.LAZY)
     @OnDelete(action = OnDeleteAction.CASCADE)
     @JoinColumn(name = "exam_reference", referencedColumnName = "id_exam")
     private Exam examReference;
 
+    /**
+     * Reference to the room in which the exam run take place
+     */
     @ManyToOne(fetch = FetchType.LAZY)
     @OnDelete(action = OnDeleteAction.CASCADE)
     @JoinColumn(name = "room_reference", referencedColumnName = "id_room")
@@ -44,9 +65,20 @@ public class ExamRun {
 //    @OneToMany(mappedBy = "examRun")
 //    private Set<BlockOnExamRun> blocksOnRun;
 
+    /**
+     * Create a new exam run
+     */
     public ExamRun() {
     }
 
+    /**
+     * Create a new exam run
+     * @param examReference Reference to the exam to which the exam run relates
+     * @param roomReference Reference to the room in which the exam run take place
+     * @param examDate Date of exam run
+     * @param startTime Time when exam run starts
+     * @param endTime Time when exam run ends
+     */
     public ExamRun(Exam examReference, Room roomReference, String examDate, String startTime, String endTime) {
         this.examReference = examReference;
         this.roomReference = roomReference;
--- src/main/java/cz/vutbr/fit/gja/models/examRun/ExamRunRepository.java
+++ src/main/java/cz/vutbr/fit/gja/models/examRun/ExamRunRepository.java
@@ -14,12 +14,35 @@ import java.util.List;
 @Repository
 public interface ExamRunRepository extends JpaRepository<ExamRun, Integer> {
 
+    /**
+     * Gets exam run by given ID
+     * @param id Exam run ID
+     * @return ExamRun
+     */
     ExamRun findByIdExamRun(int id);
 
+    /**
+     * Gets all exam runs of given exam
+     * @param exam Exam
+     * @return All exam runs of the exam
+     */
     List<ExamRun> findAllByExamReference(Exam exam);
 
+    /**
+     * Gets all exam runs which take place in given room
+     * @param room Room
+     * @return All exam runs which take place in given room
+     */
     List<ExamRun> findAllByRoomReference(Room room);
 
+    /**
+     * Return all exam runs which are in time collision
+     * @param room Room in which exam run take place
+     * @param examDate Date of exam run
+     * @param startTime Time when exam run starts
+     * @param endTime Time when exam run ends
+     * @return Exam runs which are in time collision
+     */
     @Query("select er from ExamRun er where er.roomReference = ?1 and er.examDate = ?2 and (" +
             "(er.startTime between ?3 and ?4 or er.endTime between ?3 and ?4) or (er.startTime < ?3 and er.endTime > ?4)" +
             ")")
