9ebdd318dd66b277ca205f8d21b415b6b7a0a471
Ondřej Novák <ondrej.novak96@email.cz>
2020-01-11 11:22:32 +0100

    Commented Room package, without Room Controller

--- src/main/java/cz/vutbr/fit/gja/models/room/Room.java
+++ src/main/java/cz/vutbr/fit/gja/models/room/Room.java
@@ -9,40 +9,63 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
+/**
+ * This class represents the entity of room
+ */
 @Entity
 @Table(name = "room")
 public class Room {
 
+    /**
+     * Room ID
+     */
     @Id
     @SequenceGenerator(name = "RoomIdGenerator", sequenceName = "ROOM_SEQUENCE", allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "RoomIdGenerator")
     @Column(name = "id_room")
     private int idRoom;
 
+    /**
+     * Room number
+     */
     @NotEmpty(message = "Jméno místnosti je povinné")
     @NotNull(message="Jméno místnosti je povinné")
     @Column(name = "room_number")
     private String roomNumber;
 
+    /**
+     * Number of rows in the room
+     */
     @Min(value=1, message="Počet řad musí být větší než 0")
     @Column(name = "number_of_rows")
     private int numberOfRows;
 
+    /**
+     * Number of columns in the room
+     */
     @Min(value=1, message="Počet bloků v řadě musí být větší než 0")
     @Column(name = "number_of_columns")
     private int numberOfColumns;
 
+    /**
+     * Teacher reference (who created the room)
+     */
     @ManyToOne(fetch = FetchType.LAZY)
     @OnDelete(action = OnDeleteAction.CASCADE)
     @JoinColumn(name = "teacher_reference", referencedColumnName = "email")
     private Teacher teacherReference;
 
-//    @OneToMany(mappedBy = "roomNumber")
-//    private Set<ExamRun> exam_runs;
-
     public Room() {
     }
 
+    /**
+     * Creates a new Room
+     *
+     * @param roomNumber Room number
+     * @param numberOfRows  Number of rows in the room
+     * @param numberOfColumns Number of columns in the room
+     * @param teacherReference Teacher reference
+     */
     public Room(String roomNumber, int numberOfRows, int numberOfColumns, Teacher teacherReference) {
         this.roomNumber = roomNumber;
         this.numberOfRows = numberOfRows;
--- src/main/java/cz/vutbr/fit/gja/models/room/RoomRepository.java
+++ src/main/java/cz/vutbr/fit/gja/models/room/RoomRepository.java
@@ -5,15 +5,39 @@ import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
 
+/**
+ * Repository class of Room
+ */
 @Repository
 public interface RoomRepository extends JpaRepository<Room, Integer> {
+
+    /**
+     * Finds room by room id
+     * @param id Room id
+     * @return Room if some is found, null otherwise
+     */
     Room findByIdRoom(int id);
 
+    /**
+     * Finds room by room number
+     * @param roomNumber Room number
+     * @return Room if some is found, null otherwise
+     */
     Room findByRoomNumber(String roomNumber);
 
+    /**
+     * Deletes specific room from database by id
+     * @param id Room id
+     * @return Number of deleted rooms
+     */
     @Transactional
     Long deleteByIdRoom(int id);
 
+    /**
+     * Deletes specific room from database by room number
+     * @param roomNumber Room number
+     * @return Number of deleted rooms
+     */
     @Transactional
     Long deleteByRoomNumber(String roomNumber);
 }
